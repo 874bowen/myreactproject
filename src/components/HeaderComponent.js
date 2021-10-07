af784abd8356cbc68275f4d7344d7cbf51a1fc20
Ivan Bowen <ivanovicbow850@gmail.com>
2021-10-07 07:29:47 +0300

    React Redux Form Validation

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
-import { Nav, Navbar, NavbarBrand, NavbarToggler, Collapse, NavItem, Jumbotron } from 'reactstrap';
+import { Navbar, NavbarBrand, Nav, NavbarToggler, Collapse, NavItem, Jumbotron,
+    Button, Modal, ModalHeader, ModalBody,
+    Form, FormGroup, FormFeedback, Input, Label } from 'reactstrap';
 import { NavLink } from 'react-router-dom';
 
 class Header extends Component {
@@ -8,8 +10,52 @@ class Header extends Component {
 
         this.toggleNav = this.toggleNav.bind(this);
         this.state = {
-          isNavOpen: false
+          username: '',
+          isNavOpen: false,
+          isModalOpen: false,
+          touched:{
+            username: false
+          }
         };
+        this.toggleNav=this.toggleNav.bind(this);
+        this.toggleModal = this.toggleModal.bind(this);
+        this.handleLogin = this.handleLogin.bind(this);
+      }
+
+      toggleModal() {
+        this.setState({
+          isModalOpen: !this.state.isModalOpen
+        });
+      }
+
+      handleLogin(event) {
+        this.toggleModal();
+        alert("Username: " + this.username.value + " Password: " + this.password.value
+            + " Remember: " + this.remember.checked);
+        event.preventDefault();
+
+      }
+
+      handleSubmit(event) {
+          console.log('Current State is: ' + JSON.stringify(this.state));
+          alert('Current State is: ' + JSON.stringify(this.state));
+          event.preventDefault();
+      }
+      handleBlur = (field) => (evt) => {
+         this.setState({
+             touched: { ...this.state.touched, [field]: true }
+         });
+     }
+
+      validate(username) {
+        const errors = {
+          username: ''
+        }
+        if (this.state.touched.username && username.length < 5)
+          errors.username = 'username should be greater than 4';
+        else if (this.state.touched.username && username.length > 10)
+          errors.username = 'username should be lesser than 11';
+        return errors;
       }
 
       toggleNav() {
@@ -19,8 +65,9 @@ class Header extends Component {
       }
 
     render() {
+      const errors = this.validate(this.state.username);
         return(
-            <div>
+          <React.Fragment>
                 <Navbar dark expand="md">
                     <div className="container">
                         <NavbarToggler onClick={this.toggleNav} />
@@ -39,6 +86,12 @@ class Header extends Component {
                             <NavItem>
                                 <NavLink className="nav-link" to='/contactus'><span className="fa fa-address-card fa-lg"></span> Contact Us</NavLink>
                             </NavItem>
+
+                            </Nav>
+                            <Nav className="ml-auto" navbar>
+                                <NavItem>
+                                    <Button outline onClick={this.toggleModal}><span className="fa fa-sign-in fa-lg"></span> Login</Button>
+                                </NavItem>
                             </Nav>
                         </Collapse>
                     </div>
@@ -53,7 +106,37 @@ class Header extends Component {
                         </div>
                     </div>
                 </Jumbotron>
-            </div>
+                <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+                  <ModalHeader toggle={this.toggleModal}>Login</ModalHeader>
+                  <ModalBody>
+                        <Form>
+                            <FormGroup>
+                                <Label htmlFor="username">Username</Label>
+                                <Input type="text" id="username" name="username"
+                                    innerRef={(input) => this.username = input}
+                                    alid={errors.username === ''}
+                                    invalid={errors.username !== ''}
+                                    onBlur={this.handleBlur('username')}
+                                    onChange={this.handleInputChange} />
+                                <FormFeedback>{errors.username}</FormFeedback>
+                            </FormGroup>
+                            <FormGroup>
+                                <Label htmlFor="password">Password</Label>
+                                <Input type="password" id="password" name="password"
+                                    innerRef={(input) => this.password = input}  />
+                            </FormGroup>
+                            <FormGroup check>
+                                <Label check>
+                                    <Input type="checkbox" name="remember"
+                                    innerRef={(input) => this.remember = input}  />
+                                    Remember me
+                                </Label>
+                            </FormGroup>
+                            <Button type="submit" value="submit" color="primary">Login</Button>
+                        </Form>
+                  </ModalBody>
+                </Modal>
+              </React.Fragment>
         );
     }
 }
