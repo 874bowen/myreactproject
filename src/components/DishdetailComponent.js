ac11a2e6d4e327215454156bd50675b80662bac9
Ivan Bowen <ivanovicbow850@gmail.com>
2021-12-20 09:29:30 +0300

    redux-thunk and redux-logger

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -4,7 +4,7 @@ import { Card, CardImg, CardText, CardBody, CardTitle, Button, Breadcrumb, Bread
   Form, FormGroup, FormFeedback, Input, Label  } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import {LocalForm, Control, Errors} from 'react-redux-form';
-
+import { Loading } from './LoadingComponent';
 
   function RenderDish({dish}) {
     return(
@@ -57,7 +57,27 @@ import {LocalForm, Control, Errors} from 'react-redux-form';
 
 
   const  DishDetail = (props) => {
-    if (props.dish != null){
+    if(props.isLoading){
+      return(
+          <div className="container">
+            <div className="row">
+            <Loading />
+            </div>
+          </div>
+      );
+    }
+
+    else if(props.errMess){
+      return(
+          <div className="container">
+            <div className="row">
+            <h4>{props.errMess}</h4>
+            </div>
+          </div>
+      );
+    }
+
+    else if (props.dish != null){
       return(
         <div className="container">
             <div className="row">
