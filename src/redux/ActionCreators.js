c537b7cf74e5bce2e9b8aa2cc383885f64ae9cab
Ivan Bowen <ivanovicbow850@gmail.com>
2021-12-23 08:07:54 +0300

    fetch handling errors

--- src/redux/ActionCreators.js
+++ src/redux/ActionCreators.js
@@ -15,9 +15,24 @@ export const addComment = (dishId, rating, author, comment) => ({
 export const fetchDishes = () => (dispatch) =>{
     dispatch(dishesLoading(true));
 
-    return fetch(baseUrl + 'dishes')
+    return fetch(baseUrl + 'dishees')
+        .then(response =>{
+          if (response.ok){
+            return response;
+          }
+          else {
+            var error = new Error('Error'+response.status + ': '+ response.statusText);
+            error.response = response;
+            throw error;
+          }
+        },
+          error =>{
+          var errmess = new Error(error.message);
+          throw error;
+        })
         .then(response => response.json())
-        .then(dishes => dispatch(addDishes(dishes)));
+        .then(dishes => dispatch(addDishes(dishes)))
+        .catch(error => dispatch(dishesFailed(error.message)));
     // setTimeout(()=> {
     //   dispatch(addDishes(DISHES));
     // }, 2000)
@@ -40,9 +55,23 @@ export const addDishes = (dishes) => ({
 export const fetchComments = () => (dispatch) =>{
 
     return fetch(baseUrl + 'comments')
+    .then(response =>{
+      if (response.ok){
+        return response;
+      }
+      else {
+        var error = new Error('Error'+response.status + ': '+ response.statusText);
+        error.response = response;
+        throw error;
+      }
+      },
+        error =>{
+        var errmess = new Error(error.message);
+        throw error;
+      })
         .then(response => response.json())
-        .then(comments => dispatch(addComments(comments)));
-
+        .then(comments => dispatch(addComments(comments)))
+        .catch(error => dispatch(commentsFailed(error.message)));
       }
 
 export const commentsFailed = (errmess) => ({
@@ -59,8 +88,23 @@ export const fetchPromos = () => (dispatch) =>{
     dispatch(promosLoading(true));
 
     return fetch(baseUrl + 'promotions')
+    .then(response =>{
+      if (response.ok){
+        return response;
+      }
+      else {
+        var error = new Error('Error'+response.status + ': '+ response.statusText);
+        error.response = response;
+        throw error;
+      }
+      },
+        error =>{
+        var errmess = new Error(error.message);
+        throw error;
+      })
         .then(response => response.json())
-        .then(promos => dispatch(addPromos(promos)));
+        .then(promos => dispatch(addPromos(promos)))
+        .catch(error => dispatch(promosFailed(error.message)));
 
 }
 
