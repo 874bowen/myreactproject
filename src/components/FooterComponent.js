079081d689cffd321cc1fa7976072cbe7e90d465
Ivan Bowen <ivanovicbow850@gmail.com>
2022-02-11 22:14:20 +0300

    minor changes

--- src/components/FooterComponent.js
+++ src/components/FooterComponent.js
@@ -20,11 +20,11 @@ function Footer(props) {
                 <div className="col-7 col-sm-5">
                     <h5>Our Address</h5>
                     <address>
-		              121, Clear Water Bay Road<br />
-		              Clear Water Bay, Kowloon<br />
-		              HONG KONG<br />
-		              <i className="fa fa-phone fa-lg"></i>: +852 1234 5678<br />
-		              <i className="fa fa-fax fa-lg"></i>: +852 8765 4321<br />
+		              30100, Eldoret<br />
+		              Eldoret, Uasin Gishu<br />
+		              Kenya<br />
+		              <i className="fa fa-phone fa-lg"></i>: +254 791 440095<br />
+		              <i className="fa fa-fax fa-lg"></i>: +254 791 440095<br />
 		              <i className="fa fa-envelope fa-lg"></i>: <a href="mailto:confusion@food.net">
                          confusion@food.net</a>
                     </address>
@@ -42,7 +42,7 @@ function Footer(props) {
             </div>
             <div className="row justify-content-center">
                 <div className="col-auto">
-                    <p>© Copyright 2018 Ristorante Con Fusion</p>
+                    <p>© Copyright 2022 Restaurant Con Fusion</p>
                 </div>
             </div>
         </div>
